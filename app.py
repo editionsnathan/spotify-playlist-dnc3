270b02e4b3343f9c3d13f5c5ff69f9cfb90e4a78
editionsnathan <moulinnathan@gmail.com>
2025-04-07 19:24:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,6 @@ def validate(index):
     return redirect(url_for("admin"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
